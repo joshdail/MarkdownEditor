0211babb5f830f414c45258f05595cd9639b72f7
josh <josh@Joshuas-MacBook-Air.local>
2025-05-15 20:37:49 -0400

    Initial commit

---
+++ Main.fs
@@ -0,0 +1,43 @@
+module Main
+
+open Avalonia.FuncUI.DSL
+open Avalonia.Controls
+open Avalonia.Controls.Primitives
+open Avalonia.Layout
+open Avalonia.FuncUI.Types
+open Avalonia.Media
+open Markdig
+open Elmish
+
+type Model =
+    { Text: string }
+
+type Msg =
+    | TextChanged of string
+
+let init () : Model * Cmd<Msg> =
+    { Text = "" }, Cmd.none
+
+let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
+    match msg with
+    | TextChanged newText -> {model with Text = newText}, Cmd.none
+
+let view (model: Model) (dispatch: Msg -> unit) =
+    Grid.create [
+        // Grid.rowDefinitions "Auto,*"
+        Grid.children [
+            TextBox.create [
+                Grid.row 0
+                TextBox.text model.Text
+                TextBox.acceptsReturn true
+                TextBox.verticalScrollBarVisibility ScrollBarVisibility.Auto
+                TextBox.horizontalScrollBarVisibility ScrollBarVisibility.Auto
+                TextBox.onTextChanged (TextChanged >> dispatch)
+                TextBox.fontFamily "sans serif"
+                TextBox.fontSize 14.0
+                TextBox.padding 10.0
+                TextBox.horizontalAlignment HorizontalAlignment.Stretch
+                TextBox.verticalAlignment VerticalAlignment.Stretch
+            ]
+        ]
+    ]
---
+++ Program.fs
@@ -0,0 +1,38 @@
+﻿open Avalonia
+open Avalonia.Controls
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.FuncUI.Hosts
+open Avalonia.FuncUI.Elmish
+open Avalonia.Themes.Fluent
+open Avalonia.Styling
+open Elmish
+
+type MainWindow() as this = 
+    inherit HostWindow()
+    do
+        base.Title <- "Markdown Editor"
+        base.Width <- 800.0
+        base.Height <- 600.0
+
+        Program.mkProgram Main.init Main.update Main.view
+        |> Program.withHost this
+        |> Program.run
+
+type App() =
+    inherit Application()
+
+    override this.Initialize () =
+        this.Styles.Add(FluentTheme())
+    override this.OnFrameworkInitializationCompleted (): unit = 
+            match this.ApplicationLifetime with
+            | :? IClassicDesktopStyleApplicationLifetime as desktop ->
+                desktop.MainWindow <- MainWindow()
+            | _ -> ()
+
+[<EntryPoint>]
+    let main argv =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace()
+            .StartWithClassicDesktopLifetime(argv)
